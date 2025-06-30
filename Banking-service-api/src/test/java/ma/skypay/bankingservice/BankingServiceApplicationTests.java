e215152ba2be8fb5e90d9c756f5ac6de48e242a6
ISSAM EL ORF <issam.el-orf@inetum.com>
2025-06-30 15:16:51 +0100

    feature: Start Banking service api - Create domain,

---
+++ Banking-service-api/src/test/java/ma/skypay/bankingservice/BankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.skypay.bankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
